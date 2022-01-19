8e9542d2dda6333b6d06003f5942d86e4a98ec9b
Diego Socualaya <diegosruiz18@gmail.com>
2022-01-19 11:33:56 -0500

    Agregando modales y pantalla Mostrar Ticket de modulo Reserva

--- app/src/main/java/com/example/digiticket_app_01/ReservaResumenReserva.java
+++ app/src/main/java/com/example/digiticket_app_01/ReservaResumenReserva.java
@@ -2,13 +2,31 @@ package com.example.digiticket_app_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ReservaResumenReserva extends AppCompatActivity {
+
+    //variables para el modal
+    private Button btnReservar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserva_resumen_reserva);
+
+        //linkeando el modal con el boton
+        btnReservar = findViewById(R.id.btn_reservar_RR);
+        //creando listener
+        btnReservar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //iniciando el modal
+                startActivity(new Intent(ReservaResumenReserva.this, ModalConfirmacion.class));
+            }
+        });
     }
 }
