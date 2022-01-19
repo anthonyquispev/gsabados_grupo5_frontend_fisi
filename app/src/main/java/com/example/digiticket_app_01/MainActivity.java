5eea8c8d35950c07c20414311248665f677cde56
Diego Socualaya <diegosruiz18@gmail.com>
2022-01-19 14:14:39 -0500

    Agregando pantalla Elegir Turno en modulo Reserva

--- app/src/main/java/com/example/digiticket_app_01/MainActivity.java
+++ app/src/main/java/com/example/digiticket_app_01/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reserva_turno);
+        setContentView(R.layout.activity_main);
 
         Retrofit retrofitClient = RetrofitClient.getInstance();
         iMyService = retrofitClient.create(IMyService.class);
