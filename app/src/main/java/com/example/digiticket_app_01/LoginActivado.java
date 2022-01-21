789b083aab03df04accee8ffe071762dde4caffe
Diego Socualaya <diegosruiz18@gmail.com>
2022-01-21 02:41:38 -0500

    Agregando pantalla de Login (codigo y activado) y formulario de Registro de usuario

---
+++ app/src/main/java/com/example/digiticket_app_01/LoginActivado.java
@@ -0,0 +1,19 @@
+package com.example.digiticket_app_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+
+public class LoginActivado extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activado);
+
+
+    }
+}
